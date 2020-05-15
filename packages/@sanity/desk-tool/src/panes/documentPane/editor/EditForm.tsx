a05683768890d7647b9e38295ccfcd438a29d084
Bjørge Næss <bjoerge@gmail.com>
2020-05-15 14:27:55 +0200

    Restore directory with correct casing

---
+++ packages/@sanity/desk-tool/src/panes/documentPane/editor/EditForm.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {FormBuilder} from 'part:@sanity/form-builder'
+import {Overlay as PresenceOverlay} from '@sanity/components/presence'
+
+import styles from '../Editor.css'
+import {Subscription} from 'rxjs'
+
+const preventDefault = ev => ev.preventDefault()
+type Doc = any
+type Schema = any
+type SchemaType = any
+
+interface Props {
+  id: string
+  value: Doc
+
+  filterField: () => boolean
+  focusPath: []
+  markers: []
+
+  onBlur: () => void
+  onChange: (event: any) => void
+  onFocus: (focusPath: any[]) => void
+  readOnly: boolean
+  schema: Schema
+  type: SchemaType
+  presence: any
+}
+
+export default class EditForm extends React.PureComponent<Props> {
+  subscription?: Subscription
+  patchChannel = FormBuilder.createPatchChannel()
+
+  componentWillUnmount() {
+    if (this.subscription) this.subscription.unsubscribe()
+  }
+
+  render() {
+    const {
+      filterField,
+      focusPath,
+      markers,
+      value,
+      onBlur,
+      onFocus,
+      onChange,
+      readOnly,
+      schema,
+      type,
+      presence
+    } = this.props
+    return (
+      <PresenceOverlay margin={[0, 0, 0, 0]}>
+        <form className={styles.editor} onSubmit={preventDefault}>
+          <FormBuilder
+            schema={schema}
+            patchChannel={this.patchChannel}
+            value={value || {_type: type}}
+            type={type}
+            presence={presence}
+            filterField={filterField}
+            readOnly={readOnly}
+            onBlur={onBlur}
+            onFocus={onFocus}
+            focusPath={focusPath}
+            onChange={onChange}
+            markers={markers}
+          />
+        </form>
+      </PresenceOverlay>
+    )
+  }
+}
---
+++ packages/@sanity/desk-tool/src/panes/documentPane/editor/history.js
@@ -0,0 +1,4 @@
+const BREAKPOINT_SCREEN_MEDIUM = 512
+
+export const HISTORY_LOADING = Symbol.for('HISTORY_LOADING')
+export const historyIsEnabled = () => window && window.innerWidth > BREAKPOINT_SCREEN_MEDIUM
